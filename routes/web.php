771fa2429c8d556c1316072c6588e7123a098294
Jesus <jesus.cuschieri.a100878@mcast.edu.mt>
2025-02-27 10:07:59 +0000

    Added resource routes for colleges and students

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CollegeController;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('colleges.index');
 });
+
+Route::resource('colleges', CollegeController::class);
+Route::resource('students', StudentController::class);
+

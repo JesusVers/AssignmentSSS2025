59aaf2a581e1fcd06c53d087b909f6ef91e20333
Jesus <jesus.cuschieri.a100878@mcast.edu.mt>
2025-03-03 13:19:04 +0000

    Resolved merge conflicts

--- resources/views/colleges/index.blade.php
+++ resources/views/colleges/index.blade.php
@@ -1,36 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>Colleges</h2>
-    <a href="{{ route('colleges.create') }}" class="btn btn-primary mb-3">Add College</a>
+<div class="container mt-4">
+    <div class="card">
+        <div class="card-header bg-primary text-white">
+            <h2>Colleges</h2>
+        </div>
+        <div class="card-body">
+            <a href="{{ route('colleges.create') }}" class="btn btn-success mb-3">Add College</a>
 
-    @if (session('success'))
-        <div class="alert alert-success">{{ session('success') }}</div>
-    @endif
+            @if (session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
 
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Address</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($colleges as $college)
-                <tr>
-                    <td>{{ $college->name }}</td>
-                    <td>{{ $college->address }}</td>
-                    <td>
-                        <a href="{{ route('colleges.edit', $college->id) }}" class="btn btn-warning">Edit</a>
-                        <form action="{{ route('colleges.destroy', $college->id) }}" method="POST" class="d-inline">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+            <!-- College Table -->
+            <table class="table table-striped table-hover">
+                <thead class="table-dark">
+                    <tr>
+                        <th>Name</th>
+                        <th>Address</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($colleges as $college)
+                        <tr>
+                            <td>{{ $college->name }}</td>
+                            <td>{{ $college->address }}</td>
+                            <td>
+                                <a href="{{ route('colleges.edit', $college->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                                <form action="{{ route('colleges.destroy', $college->id) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,21 @@ use App\Http\Controllers\CollegeController;
 use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
-    return redirect()->route('colleges.index');
+    return redirect()->route('college.index');
 });
 
-Route::resource('colleges', CollegeController::class);
-Route::resource('students', StudentController::class);
+// College Routes (Singular)
+Route::get('/college', [CollegeController::class, 'index'])->name('college.index');
+Route::get('/college/create', [CollegeController::class, 'create'])->name('college.create');
+Route::post('/college', [CollegeController::class, 'store'])->name('college.store');
+Route::get('/college/{id}/edit', [CollegeController::class, 'edit'])->name('college.edit');
+Route::put('/college/{id}', [CollegeController::class, 'update'])->name('college.update');
+Route::delete('/college/{id}', [CollegeController::class, 'destroy'])->name('college.destroy');
 
+// Student Routes (Singular)
+Route::get('/student', [StudentController::class, 'index'])->name('student.index');
+Route::get('/student/create', [StudentController::class, 'create'])->name('student.create');
+Route::post('/student', [StudentController::class, 'store'])->name('student.store');
+Route::get('/student/{id}/edit', [StudentController::class, 'edit'])->name('student.edit');
+Route::put('/student/{id}', [StudentController::class, 'update'])->name('student.update');
+Route::delete('/student/{id}', [StudentController::class, 'destroy'])->name('student.destroy');
